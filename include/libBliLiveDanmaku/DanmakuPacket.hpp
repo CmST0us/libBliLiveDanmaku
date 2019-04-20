184ebc8821a34f2c0da45f214e21b355ea8ca516
eric3u <eric3u@outlook.com>
2019-04-20 20:55:24 +0800

    [Feat]: Add Heartbeat Command Auth Packet

--- include/libBliLiveDanmaku/DanmakuPacket.hpp
+++ include/libBliLiveDanmaku/DanmakuPacket.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <format.hpp>
+
 namespace blilive
 {
 
@@ -21,7 +21,7 @@ public:
     DanmakuPacket();
     virtual ~DanmakuPacket();
     virtual uint32_t packetSize();
-    virtual blilive::PacketOperationCode operationCode();
+    virtual PacketOperationCode operationCode();
     uint16_t mHeaderSize = 16;
     uint16_t mVersion = 1;
     uint32_t mSequence = 1;
@@ -30,6 +30,43 @@ public:
 
 
 }; // DanmakuPacket
+
+class DanmakuAuthPacket: public DanmakuPacket {
+public:
+    int mRoomID = 0;
+    virtual PacketOperationCode operationCode() override;
+    virtual std::string data() override;
+}; // DanmakuAuthPacket
+
+class DanmakuHeartbeatPacket: public DanmakuPacket {
+public:
+    virtual PacketOperationCode operationCode() override;
+}; // DanmakuHeartbeatPacket;
+
+class DanmakuCommandPacket: public DanmakuPacket {
+public:
+    struct Command {
+        static const std::string RecvDanmaku;
+        static const std::string SendGift;
+        static const std::string Welcome;
+        static const std::string WelcomeGuard;
+        static const std::string SystemMessage;
+        static const std::string Preparing;
+        static const std::string Live;
+        static const std::string WishBottle;
+
+        // Inner Command, Not API
+        static const std::string Unsupport;
+    };
+
+    DanmakuCommandPacket(std::string& jsonStr);
+    virtual PacketOperationCode operationCode() override;
+    std::string command();
+
+protected:
+    std::string _jsonString = "";
+};
+
 
 } //blilive
 
--- src/DanmakuPacket.cpp
+++ src/DanmakuPacket.cpp
@@ -1,4 +1,5 @@
 #include <format.hpp>
+#include <json/json.h>
 #include "DanmakuPacket.hpp"
 
 blilive::DanmakuPacket::DanmakuPacket() {
@@ -31,4 +32,59 @@ blilive::PacketOperationCode blilive::DanmakuPacket::operationCode() {
 
 std::string blilive::DanmakuPacket::data() {
     return std::string();
+}
+
+#pragma mark - Danmaku Auth Pack
+blilive::PacketOperationCode blilive::DanmakuAuthPacket::operationCode() {
+    return blilive::PacketOperationCode::Auth;
+}
+
+std::string blilive::DanmakuAuthPacket::data() {
+    Json::Value root;
+    root["uid"] = Json::Int(0);
+    root["roomid"] = Json::Int(this->mRoomID);
+    root["protover"] = Json::Int(2);
+    root["platform"] = Json::StaticString("web");
+    root["clientver"] = Json::StaticString("1.4.0");
+    return root.toStyledString();
+}
+
+#pragma mark - Danmaku Heartbeat Packet
+blilive::PacketOperationCode blilive::DanmakuHeartbeatPacket::operationCode() {
+    return blilive::PacketOperationCode::ClientHeartbeat;
+}
+
+#pragma mark - Danmaku Command Packet
+const std::string blilive::DanmakuCommandPacket::Command::RecvDanmaku("DANMU_MSG");
+const std::string blilive::DanmakuCommandPacket::Command::SendGift("SEND_GIFT");
+const std::string blilive::DanmakuCommandPacket::Command::Welcome("WELCOME");
+const std::string blilive::DanmakuCommandPacket::Command::WelcomeGuard("WELCOME_GUARD");
+const std::string blilive::DanmakuCommandPacket::Command::SystemMessage("SYS_MSG");
+const std::string blilive::DanmakuCommandPacket::Command::Preparing("PREPARING");
+const std::string blilive::DanmakuCommandPacket::Command::Live("LIVE");
+const std::string blilive::DanmakuCommandPacket::Command::WishBottle("WISH_BOTTLE");
+
+// Inner Command, Not API
+const std::string blilive::DanmakuCommandPacket::Command::Unsupport("_Unsupport");
+
+blilive::DanmakuCommandPacket::DanmakuCommandPacket(std::string& jsonStr) {
+    this->_jsonString = jsonStr;
+}
+
+blilive::PacketOperationCode blilive::DanmakuCommandPacket::operationCode() {
+    return blilive::PacketOperationCode::Command;
+}
+
+std::string blilive::DanmakuCommandPacket::command() {
+    Json::CharReaderBuilder b;
+    Json::CharReader *jsonReader(b.newCharReader());
+    Json::Value jsonRoot;
+    JSONCPP_STRING errs;
+    const char *pStr = this->_jsonString.c_str();
+    bool success = jsonReader->parse(pStr, pStr + strlen(pStr), &jsonRoot, &errs);
+    delete jsonReader;
+    if (!success) {
+        return blilive::DanmakuCommandPacket::Command::Unsupport;
+    }
+    return jsonRoot["cmd"].asString();
 }
